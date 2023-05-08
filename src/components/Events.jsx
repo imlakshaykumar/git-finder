3f3b41fa88d5c467a6c2f287fd63211a01823964
Lakshay Kumar <imlakshaykumar@gmail.com>
2023-05-08 19:29:00 +0530

    added activity history

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -1,7 +1,44 @@
-export const Events = ({ eventsInfo }) => {
-    console.log(eventsInfo);
+import '../style/events.css'
+import Axios from 'axios';
+import { useEffect, useState } from 'react';
+
+export const Events = ({ type }) => {
+    // console.log(type);
+
+    let baseURL = "https://api.github.com/users";
+    let pathname = window.location.pathname;
+    let [events, setEvents] = useState([])
+
+    async function getEvents() {
+        const res = await Axios.get(baseURL + pathname + "/" + `${type}`)
+        setEvents(res.data);
+    }
+    useEffect(() => {
+        getEvents()
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [setEvents])
+
+
     return (
-        <p>Events</p>
+        <div className="eventsMainDiv">
+            {
+                events && events?.map((data, key) => {
+                    return (
+                        <div className="eventsDiv" key={ key }>
+                            <img src={ data.actor.avatar_url } alt="avatar url" className='avatar' />
+                            <div className="eventInfoDiv">
+                                <p>
+                                    { data.actor.login }
+                                    { data.type }
+                                </p>
+                                <p>{ data.repo.name }</p>
+                            </div>
+                        </div>
+
+                    )
+                })
+            }
+        </div>
     )
 }
 
