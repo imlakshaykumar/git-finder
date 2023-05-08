31240e75bec0812a2f6f9989ce47d139254d0a97
Lakshay Kumar <imlakshaykumar@gmail.com>
2023-05-08 19:56:53 +0530

    added Link to user

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -1,6 +1,7 @@
 import '../style/events.css'
 import Axios from 'axios';
 import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 export const Events = ({ type }) => {
     // console.log(type);
@@ -25,7 +26,13 @@ export const Events = ({ type }) => {
                 events && events?.map((data, key) => {
                     return (
                         <div className="eventsDiv" key={ key }>
-                            <img src={ data.actor.avatar_url } alt="avatar url" className='avatar' />
+                            <Link to={ `/${data.actor.login}` } >
+                                <img
+                                    src={ data.actor.avatar_url }
+                                    alt="avatar url"
+                                    className='avatar'
+                                />
+                            </Link>
                             <div className="eventInfoDiv">
                                 <p>
                                     { data.actor.login }
