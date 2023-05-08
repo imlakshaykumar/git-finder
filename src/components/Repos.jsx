770f07a8de7c4b428fc5f5e8bec9483bb2fbc395
Lakshay Kumar <imlakshaykumar@gmail.com>
2023-05-08 19:28:21 +0530

    user repos fetch component

--- src/components/Repos.jsx
+++ src/components/Repos.jsx
@@ -1,8 +1,42 @@
-export const Repos = ({ reposInfo }) => {
-    console.log(reposInfo);
+import Axios from 'axios';
+import { useEffect, useState } from 'react';
+import '../style/repos.css'
+
+export const Repos = ({ type }) => {
+    // console.log(type);
+    let baseURL = "https://api.github.com/users";
+    let pathname = window.location.pathname;
+    let [gitrepos, setGitrepos] = useState([])
+
+    async function getRepos() {
+        const res = await Axios.get(baseURL + pathname + "/" + `${type}`)
+        setGitrepos(res.data);
+    }
+    useEffect(() => {
+        getRepos()
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [setGitrepos])
+
     return (
-        <div>
-            <p>Repos</p>
+        <div className="repoMainDiv">
+            {
+                gitrepos && gitrepos?.map((data, key) => {
+                    return (
+                        <div key={ key } className="repoDiv">
+                            <p className='repoLink' onClick={ () => {
+                                window.open(data.html_url, "_blank")
+                            } }>
+                                { data.full_name }
+                            </p>
+                            <p className='repoDetail'>
+                                <span>🟢{ data.language }</span>
+                                <span>forks:{ data.forks }</span>
+                                <span>stars:{ data.stargazers_count }</span>
+                            </p>
+                        </div>
+                    )
+                })
+            }
         </div>
     )
 }
