67c66edbc69f0e8929d7a473ddd508b5f30cfae7
Lakshay Kumar <imlakshaykumar@gmail.com>
2023-05-08 19:29:27 +0530

    show user followers

--- src/components/followers.jsx
+++ src/components/followers.jsx
@@ -1,9 +1,26 @@
+import Axios from 'axios'
+import { useEffect, useState } from 'react';
+import { UserContainer } from './usersContainer';
 
-export const Followers = ({ followersInfo }) => {
-    console.log(followersInfo);
+export const Followers = ({ type }) => {
+    // console.log(type);
+
+    let baseURL = "https://api.github.com/users";
+    let pathname = window.location.pathname;
+    let [followers, setFollowers] = useState([])
+
+    async function getFollowers() {
+        const res = await Axios.get(baseURL + pathname + "/" + `${type}`)
+        setFollowers(res.data);
+    }
+    useEffect(() => {
+        getFollowers()
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [setFollowers])
+
     return (
         <div>
-            <p>Followers</p>
+            <UserContainer users={ followers } />
         </div>
     );
 };
